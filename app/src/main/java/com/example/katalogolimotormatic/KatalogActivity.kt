b1a83e6b641fa9099fe5a9763d3bc8cebd87ebe8
indriasten <astenindriani@gmail.com>
2026-04-21 14:42:23 +0800

    implementasi struktur awal project dan desain antarmuka home, katalog, detail, serta layout portrait-landscape

--- app/src/main/java/com/example/katalogolimotormatic/KatalogActivity.kt
+++ app/src/main/java/com/example/katalogolimotormatic/KatalogActivity.kt
@@ -1,4 +1,12 @@
-package com.example.katalogolimotormatic
+package com.example.katalog_olimotormatic
 
-class KatalogActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.katalogolimotormatic.R
+
+class KatalogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_katalog)
+    }
 }
